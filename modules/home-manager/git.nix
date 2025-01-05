41d4a45c0ddd41dfae51c0b142207d9518598e18
Vadim Pashkov <vadimpashkov.job@gmail.com>
2025-01-05 05:22:34 +0300

    fix(git): update git.nix file

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,5 +3,15 @@
     enable = true;
     userName = "Vadim Pashkov";
     userEmail = "vadimpashkov.job@gmail.com";
+    aliases = {
+      save = "!git add -A && git commit -m 'SAVEPOINT'";
+      wip = "commit -am 'WIP' --no-verify";
+    };
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      core.editor = "nvim";
+    };
   };
 }
