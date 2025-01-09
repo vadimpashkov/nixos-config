60eafcc022e53700f17a18db5ea25b38998ec83b
Vadim Pashkov <vadimpashkov.job@gmail.com>
2025-01-10 01:48:17 +0300

    feat(swappy): add config file

--- users/anisutsuri/home-manager/swappy.nix
+++ users/anisutsuri/home-manager/swappy.nix
@@ -1,3 +1,12 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  outputs,
+  ...
+}: {
   home.packages = with pkgs; [swappy];
+
+  home.file.".config/swappy/config".text = ''
+    save_dir="${outputs.globalConfig.dir.home}/Pictures/Screenshot"
+    save_filename_format=swappy-<time>.png
+  '';
 }
