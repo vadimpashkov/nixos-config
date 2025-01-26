d1da2d992f75ec723b6605a7ceab51b92ec399f2
Vadim Pashkov <vadimpashkov.job@gmail.com>
2025-01-26 22:11:27 +0300

    feat: add userAbsolute path to config.nix

--- config.nix
+++ config.nix
@@ -4,9 +4,9 @@ rec {
   profile = "desktop";
 
   dir = {
-    home = "/home/${username}";
+    home = "/home/${username}"; # TODO: Нужно этот путь получать из config.home.homeDirectory
     config = "${dir.home}/nixos-config";
-    shScripts = ./non-nix/sh;
+    userAbsolute = "${dir.config}/users/${username}"; # TODO: Понять бы как путь, что ниже сделать абсолютным и чтобы его использовать можно было
     user = ./users/${username};
     dotfiles = "${dir.user}/dotfiles";
   };
